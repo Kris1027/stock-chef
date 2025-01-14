83791281c17b6e8e7bd2deaad3cf8a1d94905425
kris1027 <krzys89@gmail.com>
2025-01-14 06:41:14 +0100

    feat: add Prisma integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
-const HomePage = () => {
-    return <div>HomePage</div>;
+import { prisma } from './lib/prisma';
+
+const HomePage = async () => {
+    const ingredient = await prisma.ingredient.findMany();
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <ul>
+                {ingredient.map((i) => (
+                    <li key={i.id}>{i.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default HomePage;
