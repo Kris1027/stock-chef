a20aa4c038a2fae5acf0d99d50e2e26cfc50f591
kris1027 <krzys89@gmail.com>
2025-01-16 16:48:42 +0100

    feat: integrate Combobox component for ingredient selection in SubtractIngredientsForm

--- app/_components/subtract-ingredients-form.tsx
+++ app/_components/subtract-ingredients-form.tsx
@@ -2,6 +2,7 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { SubtractIngredientsAction } from '../_actions/subtract-ingredients-action';
 import { IngredientProps } from '@/types/ingredients';
+import Combobox from './combobox';
 
 interface SubtractIngredientsFormProps {
     ingredients: IngredientProps[];
@@ -9,18 +10,14 @@ interface SubtractIngredientsFormProps {
 
 const SubtractIngredientsForm: React.FC<SubtractIngredientsFormProps> = ({ ingredients }) => {
     return (
-        <form action={SubtractIngredientsAction} className='flex flex-col gap-4'>
+        <div>
             <h2 className='text-center text-2xl font-bold'>Zużycie</h2>
-            <select name='ingredient'>
-                {ingredients.map((i) => (
-                    <option key={i.id} value={i.id}>
-                        {i.name}
-                    </option>
-                ))}
-            </select>
-            <Input type='number' name='newQuantity' placeholder='ilość w kg' />
-            <Button type='submit'>Odejmij</Button>
-        </form>
+            <form action={SubtractIngredientsAction} className='flex gap-4'>
+                <Combobox ingredients={ingredients} />
+                <Input type='number' name='newQuantity' placeholder='ilość w kg' />
+                <Button type='submit'>Odejmij</Button>
+            </form>
+        </div>
     );
 };
 
