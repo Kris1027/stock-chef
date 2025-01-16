3b59a5938338a9128e2783669bb80c92cc057dae
kris1027 <krzys89@gmail.com>
2025-01-16 14:23:25 +0100

    feat: enhance delivery and ingredient lists with improved layout and styling

--- app/_components/delivery-list.tsx
+++ app/_components/delivery-list.tsx
@@ -29,25 +29,27 @@ interface DeliveryListProps {
 
 const DeliveryList: React.FC<DeliveryListProps> = async ({ delivery }) => {
     return (
-        <Table>
-            <TableCaption>Dostawy</TableCaption>
-            <TableHeader>
-                <TableRow>
-                    <TableHead>Składnik</TableHead>
-                    <TableHead>Ilość</TableHead>
-                    <TableHead>Data dostawy</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {delivery.map((d) => (
-                    <TableRow key={d.id}>
-                        <TableCell>{d.Ingredient?.name}</TableCell>
-                        <TableCell>{d.newQuantity} gram</TableCell>
-                        <TableCell>{d.createdAt.toLocaleString()}</TableCell>
+        <div className='h-screen overflow-scroll'>
+            <Table>
+                <TableCaption className='caption-top font-semibold text-lg'>Dostawy</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Składnik</TableHead>
+                        <TableHead>Ilość</TableHead>
+                        <TableHead>Data dostawy</TableHead>
                     </TableRow>
-                ))}
-            </TableBody>
-        </Table>
+                </TableHeader>
+                <TableBody>
+                    {delivery.map((d) => (
+                        <TableRow key={d.id}>
+                            <TableCell>{d.Ingredient?.name}</TableCell>
+                            <TableCell>{d.newQuantity} gram</TableCell>
+                            <TableCell>{d.createdAt.toLocaleString()}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
     );
 };
 
--- app/_components/ingredient-list.tsx
+++ app/_components/ingredient-list.tsx
@@ -29,25 +29,29 @@ interface IngredientListProps {
 
 const IngredientList: React.FC<IngredientListProps> = async ({ ingredients }) => {
     return (
-        <Table>
-            <TableCaption>Stan Magazynowy</TableCaption>
-            <TableHeader>
-                <TableRow>
-                    <TableHead>Składnik</TableHead>
-                    <TableHead>Ilość</TableHead>
-                    <TableHead>Ostatnia dostawa</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {ingredients.map((ingredient) => (
-                    <TableRow key={ingredient.id}>
-                        <TableCell>{ingredient.name}</TableCell>
-                        <TableCell>{ingredient.quantity} gram</TableCell>
-                        <TableCell>{ingredient.updatedAt.toLocaleString()}</TableCell>
+        <div className='h-screen overflow-scroll'>
+            <Table>
+                <TableCaption className='caption-top font-semibold text-lg'>
+                    Stan Magazynowy
+                </TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Składnik</TableHead>
+                        <TableHead>Ilość</TableHead>
+                        <TableHead>Ostatnia dostawa</TableHead>
                     </TableRow>
-                ))}
-            </TableBody>
-        </Table>
+                </TableHeader>
+                <TableBody>
+                    {ingredients.map((ingredient) => (
+                        <TableRow key={ingredient.id}>
+                            <TableCell>{ingredient.name}</TableCell>
+                            <TableCell>{ingredient.quantity} gram</TableCell>
+                            <TableCell>{ingredient.updatedAt.toLocaleString()}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
     );
 };
 
